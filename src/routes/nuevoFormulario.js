6ac4f6f803c53fea25053f9b9c2464a4a0bd3df9
luisVillegas <44413936+luicho98@users.noreply.github.com>
2020-09-21 15:39:12 -0600

    Ruta para nuevo formulario
    creacion del metodo post y ruta para recibir los datos del front

---
+++ src/routes/nuevoFormulario.js
@@ -0,0 +1,16 @@
+router.post('/', (req,res)=>{
+    const{tipoSol,nomSolicitante,cedulaSolicitante,represLegalSolicitante,
+        cedulaJuriSolicitante,telSolicitante,faxSolicitante,dirSolicitante,
+        correoEleSolicitante,nomPropietario,represLegalPropietario,cedulaJuriPropietario,
+        dirPropietario,nomComercial,actividad,dirExactaLocal,numPatenteCom,
+        numPlanoCatastro,telLocal,faxLocal,estadoPaten,correoEleLocal,declaraJura,
+        nomTraspaso,nomNegocio,cedulaTraspaso,represLegalTraspaso,cedulaJuriTraspaso,telTraspaso,
+        faxTraspaso,dirTraspaso,correoEleTraspaso,nomAutorizado,cedAutorizado,message}=req.body;
+    if(tipoSol){
+        res.json('Done');
+        console.log('Entro la info');
+    }else{
+        res.status(500).json({error:'There was an error'});
+    }
+    
+});
