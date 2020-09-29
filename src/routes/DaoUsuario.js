714d1399f9b8498d645d265a089635fffd8ddc03
Carlos-2596 <56849575+Carlos-2596@users.noreply.github.com>
2020-09-29 09:21:37 -0600

    Metodo para obtener al usuario por medio del ID, se usa para consultar primero si existe dicho registro  para no agregarlo dos veces y evitar el error de choque por la llave primaria.

--- src/routes/DaoUsuario.js
+++ src/routes/DaoUsuario.js
@@ -7,6 +7,28 @@ class DaoUsuario {
         this.usuario = new Array();
     }
 
+//Metodo para obtener al usuario por medio del ID, se usa para consultar primero si existe dicho registro 
+//para no agregarlo dos veces y evitar el error de choque por la llave primaria.
+
+    obtener_Usuario(PK_ID, callback) {
+        var sql = "SELECT* FROM usuario WHERE PK_ID = ?";
+
+        this.connection.query(sql, [PK_ID], function (err, results) {
+            if (err) {
+                throw err;
+            }
+            var mostrarMensaje;
+            //Retorna un mensaje, dando permiso de insertar o no el usuario.
+            if (results.length === 0) {
+                mostrarMensaje = "No se encuentra registrado...";
+            }
+            else {
+                mostrarMensaje = "Si se encuentra registrado...";
+            }
+            return callback(mostrarMensaje);
+        })
+    }
+
     listar_Usuarios(callback) {
 
         var sql = "SELECT * FROM usuario";
