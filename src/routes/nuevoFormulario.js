dfe70592a000c736ac9b4cd3c16c55fe727b57be
luisVillegas <44413936+luicho98@users.noreply.github.com>
2020-10-07 09:36:29 -0600

    cambios del form

--- src/routes/nuevoFormulario.js
+++ src/routes/nuevoFormulario.js
@@ -15,11 +15,29 @@ router.post('/', (req,res)=>{
         dirPropietario,nomComercial,actividad,declaraJura,
         nomAutorizado,cedAutorizado}=req.body;
 
-    if(nomSolicitante&&cedulaSolicitante&&represLegalSolicitante&&
-        cedulaJuriSolicitante&&telSolicitante&&faxSolicitante&&dirSolicitante&&
-        correoEleSolicitante&&nomPropietario&&represLegalPropietario&&cedulaJuriPropietario&&
-        dirPropietario&&nomComercial&&actividad&&declaraJura&&
-        nomAutorizado&&cedAutorizado){
+    if(nomSolicitante&&cedulaSolicitante&&
+        telSolicitante&&correoEleSolicitante&&
+        nomPropietario&&
+        nomComercial&&actividad&&declaraJura
+        ){
+/*
+          if(!represLegalSolicitante || !cedulaJuriSolicitante){
+            represLegalSolicitante="NA";
+            cedulaJuriSolicitante="0";
+          }
+          if(!faxSolicitante){
+            faxSolicitante="0";
+          }
+          if(!dirSolicitante || !represLegalPropietario || !cedulaJuriPropietario || !dirPropietario){
+            dirSolicitante="NA";
+            represLegalPropietario="NA";
+            cedulaJuriPropietario="0";
+            dirPropietario="NA";
+          }
+          if(!nomAutorizado || !cedAutorizado){
+            nomAutorizado="NA";
+            cedAutorizado="0";
+          }*/
 
             soli.insertar_Solicitud_Patentes(cedulaSolicitante,"Contribuyente",nomSolicitante,telSolicitante,dirSolicitante,faxSolicitante,correoEleSolicitante,
             represLegalSolicitante,cedulaJuriSolicitante,nomPropietario,cedulaJuriPropietario,represLegalPropietario,dirPropietario,nomComercial,actividad,
