77154871364d77df5761e1bb51c8cf810b3b269d
Carlos-2596 <56849575+Carlos-2596@users.noreply.github.com>
2020-10-10 20:16:49 -0600

    Se crea el DAO Administrativo para el login, con el metodo para ingresar y para obtener.

---
+++ src/routes/DaoAdministrativo.js
@@ -0,0 +1,83 @@
+const dbConnection = require('../config/dbConnection');
+const daoUsuario = require('./DaoUsuario');
+
+class DaoAdministrativo{
+
+
+    constructor() {
+
+        this.connection = dbConnection();
+        this.administrativo = new Array();
+        this.DaoUsu = new daoUsuario();
+
+    }
+
+    obtener_Administrativo_Clave(PK_ID,Clave,callback) {
+        var sql = "SELECT* FROM administrativo WHERE PK_ID = ? and Clave = ?";
+
+        this.connection.query(sql, [PK_ID,Clave], function (err, results) {
+            if (err) {
+                throw err;
+            }
+            var mostrarMensaje;
+            if (results.length === 0) {
+                mostrarMensaje = "No se encuentra registrado...";
+            }
+            else {
+                mostrarMensaje = "Si se encuentra registrado...";
+            }
+            return callback(mostrarMensaje);
+        })
+    }
+
+    obtener_Administrativo(PK_ID,callback) {
+        var sql = "SELECT* FROM administrativo WHERE PK_ID = ? ";
+
+        this.connection.query(sql, [PK_ID], function (err, results) {
+            if (err) {
+                throw err;
+            }
+            var mostrarMensaje;
+            if (results.length === 0) {
+                mostrarMensaje = "No se encuentra registrado...";
+            }
+            else {
+                mostrarMensaje = "Si se encuentra registrado...";
+            }
+            return callback(mostrarMensaje);
+        })
+    }
+
+    insertar_Administrativo(PK_ID, Tipo, Nombre, FK_Rol, Correo, Clave,callback) {
+        this.obtener_Administrativo(PK_ID, function (result) {
+            if (result === "No se encuentra registrado...") {
+             const DaoUsu = new daoUsuario();
+                DaoUsu.insertar_Usuarios(PK_ID, Tipo, function (result) {
+                    const connection = dbConnection();
+                    var sql = "INSERT INTO administrativo SET ?";
+
+                    connection.query(sql, {
+                        PK_ID,
+                        Nombre,
+                        FK_Rol,
+                        Correo,
+                        Clave
+                    }, function (err, results) {
+                        if (err) {
+                            throw err;
+                        }
+                        var mostrarMensaje = "Se realizó con exito...";
+                        return callback(mostrarMensaje);
+
+                    })
+                });
+            }
+            else {
+                return callback("Se encuentra registrado");
+            }
+
+        });
+}
+}
+
+module.exports = DaoAdministrativo;
