1c56735c51068458c663877199dfb71b5affc076
Bryan Sánchez Brenes <bryan.jsb.1801@gmail.com>
2021-01-29 21:26:21 -0600

    fix: 🚨 se elimino en el precommit a npx eslint --fix src/* temporalmente

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,3 +14,9 @@ module.exports = {
   plugins: [],
   rules: {},
 };
+
+/*
+esta linia de codigo, activa eslint para comprobar codigo antes de hacer commit en git, la linea se inserta en el package.json en husky, se elimino por que presenta problemas
+"pre-commit": "npx prettier --write src/* && npx eslint --fix src/*"
+*/
+//
