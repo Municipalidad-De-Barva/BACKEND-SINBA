e0b44e6cd4cc3c6d354593fc1f45cad21e2323a1
Carlos-2596 <56849575+Carlos-2596@users.noreply.github.com>
2021-04-03 21:46:13 +0200

    Dao Inspeccion_Patente_Nueva: insertar_Inspecciones_Patentes_Nuevas

--- src/database/Dao_Inspeccion_Patente_Nueva.js
+++ src/database/Dao_Inspeccion_Patente_Nueva.js
@@ -22,5 +22,54 @@ export default class Dao_Inspeccion_Patente_Nueva extends dao {
     });
   }
 
+    insertar_Inspecciones_Patentes_Nuevas(
+  FK_Inspector_Administrativo,
+  FK_Solicitud_Patente,
+  Descripcion,
+  Local,
+  Direccion,
+  Requisito_Local_Acorde_Actividadl,
+  Planta_Fisica,
+  Senalizacion,
+  Luces_Emergencias,
+  Extintor,
+  Salida_Emergencia,
+    callback
+  ) {
+
+        var moment = require("moment");
+        const connection = dbConnection();
+        var sql = "INSERT INTO inspeccion_patente_nueva SET ?";
+        var myDate = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+        console.log(myDate);
+        connection.query(
+          sql,
+          {
+            FK_Inspector_Administrativo,
+            FK_Solicitud_Patente,
+            Descripcion,
+            Fecha: myDate,
+            Local,
+            Direccion,
+            Requisito_Local_Acorde_Actividadl,
+            Planta_Fisica,
+            Senalizacion,
+            Luces_Emergencias,
+            Extintor,
+            Salida_Emergencia,
+            
+          },
+          function (err, results) {
+            if (err) {
+              throw err;
+            }
+            var mostrarMensaje = "Se realizó con exito...";
+            return callback(mostrarMensaje);
+          }
+        );
+      
+    
+  }
+
 
 }
