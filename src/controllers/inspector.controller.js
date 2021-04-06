f91ec1e503484c393955cbbf6792305f22a6e418
Bryan Sánchez Brenes <bryan.jsb.1801@gmail.com>
2021-04-06 12:09:56 -0600

    apply obtener e insertar inspeccion

--- src/controllers/inspector.controller.js
+++ src/controllers/inspector.controller.js
@@ -1,7 +1,7 @@
 import daoInspector from "../database/Dao_Inspeccion_Patente_Nueva";
 const inspector = new daoInspector();
 
-export const listarInspecciones = async (req, res) => {
+export const listar_Inspecciones_Patentes_Nuevas = async (req, res) => {
   inspector.listar_Inspecciones_Patentes_Nuevas(function (result) {
     if (typeof result !== "undefined" && result !== null) {
       return res.status(200).json({list: result});
@@ -10,4 +10,51 @@ export const listarInspecciones = async (req, res) => {
   });
 
   return null;
+};
+
+export const insertar_Inspecciones_Patentes_Nuevas = async (req, res) => {
+  const {
+    FK_Inspector_Administrativo,
+    FK_Solicitud_Patente,
+    Descripcion,
+    Local,
+    Direccion,
+    Requisito_Local_Acorde_Actividadl,
+    Planta_Fisica,
+    Senalizacion,
+    Luces_Emergencias,
+    Extintor,
+    Salida_Emergencia,
+  } = req.body;
+
+  inspector.insertar_Inspecciones_Patentes_Nuevas(
+    FK_Inspector_Administrativo,
+    FK_Solicitud_Patente,
+    Descripcion,
+    Local,
+    Direccion,
+    Requisito_Local_Acorde_Actividadl,
+    Planta_Fisica,
+    Senalizacion,
+    Luces_Emergencias,
+    Extintor,
+    Salida_Emergencia,
+
+    function (result) {
+      console.log("Result:", result);
+      res.status(200).json(result);
+    }
+  );
+};
+
+export const obtener_inspeccion_patente_nueva_codigo = async (req, res) => {
+  console.log(req.body);
+
+  const {PK_Codigo_Inspeccion} = req.body;
+  inspector.obtener_inspeccion_patente_nueva_codigo(
+    PK_Codigo_Inspeccion,
+    function (result) {
+      res.status(200).json(result);
+    }
+  );
 };
--- src/routes/inspector.routes.js
+++ src/routes/inspector.routes.js
@@ -2,6 +2,22 @@ import {Router} from "express";
 const router = Router();
 import {auth} from "../middlewares";
 import * as inspectorCtrl from "../controllers/inspector.controller";
-router.post("/listar", auth.verifyToken, inspectorCtrl.listarInspecciones);
+router.get(
+  "/listar",
+  auth.verifyToken,
+  inspectorCtrl.listar_Inspecciones_Patentes_Nuevas
+);
+
+router.get(
+  "/obtener",
+  auth.verifyToken,
+  inspectorCtrl.obtener_inspeccion_patente_nueva_codigo
+);
+
+router.post(
+  "/agregar",
+  auth.verifyToken,
+  inspectorCtrl.insertar_Inspecciones_Patentes_Nuevas
+);
 
 export default router;
