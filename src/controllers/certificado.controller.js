a1143321ad44dfbad35ee6926627c847f5c9faf5
Bryan Sánchez Brenes <bryan.jsb.1801@gmail.com>
2021-05-27 23:49:17 -0600

    enviar certificado ruta

--- src/controllers/certificado.controller.js
+++ src/controllers/certificado.controller.js
@@ -1,3 +1,9 @@
+import daoPatentes from "../database/DaoSolicitud_Patente";
+const patentes = new daoPatentes();
+
 export const certificadoPatenteComercialNueva = async (req, res) => {
-  return res.status(200).json({message: "entregado"});
+  let id = req.body.id;
+  patentes.obtener_datos_para_certificado(id, function (params) {
+    res.status(200).json(params[0]);
+  });
 };
